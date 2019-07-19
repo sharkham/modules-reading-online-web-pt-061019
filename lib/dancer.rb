4bca02044187b248464407a25eab08d247f82d1d
Sam Markham <sam.e.markham@gmail.com>
2019-07-19 21:32:09 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -2,9 +2,9 @@ require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
 class Dancer
-  include Dance 
-  attr_accessor :name 
+  include Dance
+  attr_accessor :name
   def initialize(name)
-    @name = name 
+    @name = name
   end 
 end
